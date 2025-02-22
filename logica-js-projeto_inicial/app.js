39273ded09008294742c68e5e72c81b296ec614a
FeSchneider7 <fernandomoraes26@gmail.com>
2025-02-22 14:04:47 -0300

    updateAula

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -34,13 +34,41 @@
 alert('Bem vindo ao jogo: numero secreto do Schneider. Vamos ver se você consegue adivinhar qual é o número secreto que estou pensando.');
     
 let numeroSecreto = 7;
-console.log(numeroSecreto)
-let numeroChute = prompt('Digite numero para seu chute');
+console.log(numeroSecreto);
+let numeroChute;
+let tentativas = 1;
 
-if (numeroSecreto == numeroChute) {
-    alert(`Parabéns, você acertou o número secreto! ${numeroSecreto}`);
-} else {
-    alert('Que pena, você errou o número secreto!');
+//while = enquanto
+
+while (numeroChute != numeroSecreto) {
+    numeroChute = prompt('Digite um numero para seu chute');
+   
+    if (numeroSecreto == numeroChute) {
+        alert(`Parabéns, você acertou o número secreto! ${numeroSecreto} com ${tentativas} tentativas`);
+    } else { 
+         if (numeroSecreto > numeroChute) {
+            alert(`ERROU! DICA: O numero secreto é maior do que ${numeroChute}`);
+         } else {
+          alert(`ERRADO! DICA: O numero secreto é menor do que ${numeroChute}`);
+         }
+      //(Mesma coisa que abaixo) tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
 
+////////////////////////////////////////////////EXERCICIO/////////////////////////////////
 
+
+//let qtdNumeros = parseInt(prompt('Digite a quantidade de números para o cálculo da média:'));
+//let soma = 0;
+//let contador = qtdNumeros;
+
+//while (contador > 0) {
+ //   let numero = parseInt(prompt('Digite o número:'));
+  //  soma += numero;
+   // contador--; // Decrementando o contador para evitar loop infinito
+//}
+
+//let media = soma / qtdNumeros;
+//console.log(media);
+//alert(`A média dos números é: ${media}`);
